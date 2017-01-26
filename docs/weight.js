ecb11658e2ee68a601166123a5ad200d7bd9803f
Dave Pearson <davep@davep.org>
2017-01-26 16:20:28 +0000

    Use a more HTML5-like approach Also, tidy up some indentation issues in the JS.

--- docs/weight.js
+++ docs/weight.js
@@ -1,20 +1,21 @@
 function kg2st( kg ) {
-  return kg / 6.3503;
+    return kg / 6.3503;
 }
 
 function kg2stlb( kb ) {
-  var st = kg2st( kb );
-  return {
-    "st": Math.trunc( st ),
-    "lb": 14 * ( st - Math.trunc( st ) )
-  };
+    var st = kg2st( kb );
+    return {
+        "st": Math.trunc( st ),
+        "lb": 14 * ( st - Math.trunc( st ) )
+    };
 }
 
 function wordykg2stlb( kg ) {
+
+    if ( isNaN( kg ) ) {
+        return "";
+    }
+
     var stlb = kg2stlb( kg );
     return stlb.st + " st " + stlb.lb.toFixed( 2 ) + " lb";
-}
-
-function convert( document ) {
-    document.getElementById( "result" ).innerText = wordykg2stlb( document.getElementById( "kg" ).value );
 }
